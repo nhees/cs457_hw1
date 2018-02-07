3afacceb1306fa836fb95d92c13d86ca0c72987c
nhees <nhees1998@gmail.com>
2018-02-06 19:01:19 -0800

    Add files via upload
    Initial setup for files

---
+++ database.py
@@ -0,0 +1,19 @@
+
+class Parser:
+
+
+
+
+
+
+
+
+
+class Database:
+#Functions
+
+    #Constructor
+    def __init__(self):
+    #Variables
+        self.name
+        self.tableNames = [] # our array
---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    dbNames = []
+
+
+
+
+if __name__== " __main__":
+
+    main()
