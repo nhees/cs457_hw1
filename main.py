96e01c5637a84172efc72257152a4b38f8067903
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2018-02-14 08:26:26 -0800

    Standalone print-parser
    I didn't bother putting it in the class because it's only a single function anyway. Just takes input and prints out a function call. type "python3.6 main.py" then start typing commands. Ctrl+D or a .exit command will exit the program. Additionally, you can use "python3.6 main,py > input.sql" to redirect from a file. I'm fairly certain this is the input functionality they're looking for.
    
    The error handling is a bit lazy but you should at least be able to start implementing database class functions from here.

--- main.py
+++ main.py
@@ -1,12 +1,76 @@
-
-
+import sys
 
 def main():
-    dbNames = []
+	dbNames = []
 
+	try:
+		sqlInput = input()
+		while sqlInput.lower() != ".exit":
+			parse_line(sqlInput.split(";")[0])
+			sqlInput = input()
+	except EOFError:
+		pass # exit
+	print("Exiting")
 
+def parse_line(line):
+	words = line.split(" ")
 
+	if line[:2] == "--" or len(line) == 0:
+		pass #ignore
+	else:
+		try:
+			if words[0].lower() == "create":
+				if words[1].lower() == "table":
+					print("create_table(" + words[2] + ")")
 
-if __name__== " __main__":
+					print(" -- If create_table returns true:")
+					currentWord = 3
+					wordCount = len(words)
+					nextWord = words[currentWord]
 
-    main()
+					while currentWord < wordCount:
+						if words[currentWord + 1][:3] == "int":
+							print(" add_column(" + words[2] + ", int" + ", " + words[currentWord].strip("()") + ")")
+						elif words[currentWord + 1][:5] == "float":
+							print(" add_column(" + words[2] + ", float" + ", " + words[currentWord].strip("()") + ")")
+						elif words[currentWord + 1][:4] == "char":
+							print(" add_column(" + words[2] + ", char" + ", " + words[currentWord + 1][4:].strip("()") + ", " + words[currentWord].strip("()") + ")")
+						elif words[currentWord + 1][:7] == "varchar":
+							print(" add_column(" + words[2] + ", varchar" + ", " + words[currentWord + 1][7:].strip("()") + ", " + words[currentWord].strip("()") + ")")
+						currentWord += 2
+
+					print(" --")
+				elif words[1].lower() == "database":
+					print("create_database("  + words[2] + ")")
+
+			elif words[0].lower() == "drop":
+				if words[1].lower() == "table":
+					print("drop_table(" + words[2] + ")")
+				elif words[1].lower() == "database":
+					print("drop_database(" + words[2] + ")")
+
+			elif words[0].lower() == "select":
+				print("select(" + words[3] + ", " + words[1] + ")")
+
+			elif words[0].lower() == "use":
+				print("use_database(" + words[1] + ")")
+
+			elif words[0].lower() == "alter":
+				if words[1].lower() == "table":
+					if words[3].lower() == "add":
+						if words[5] == "int":
+							print("add_column(" + words[2] + ", int" + ", " + words[4] + ")")
+						elif words[5]== "float":
+							print("add_column(" + words[2] + ", float" + ", " + words[4] + ")")
+						elif words[5][:4] == "char":
+							print("add_column(" + words[2] + ", char" + ", " + words[5][4:].strip("()") + ", " + words[4] + ")")
+						elif words[5][:7] == "varchar":
+							print("add_column(" + words[2] + ", varchar" + ", " + words[5][7:].strip("()") + ", " + words[4] + ")")
+			else:
+				print("Invalid keyword: " + words[0])
+		except IndexError:
+			print("Invalid line: " + line)
+
+if __name__ == "__main__":
+	main()
+
