9fdfc6b0b9fcc5c42322d069dc2fca26f9763de2
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2018-02-27 20:25:11 -0800

    Insert implementation + many arbitrary changes

---
+++ pa2.py
@@ -0,0 +1,164 @@
+import sys
+import os
+from database import Database
+from table import Table
+
+currentDb = "NA"
+DIRECTORY = "PA2/"
+
+def main():
+	try:
+		#runs through the input and processes all of the commands
+		sqlInput = input().rstrip("\r\n")
+		while sqlInput.lower() != ".exit":
+			while sqlInput[:2] != "--" and len(sqlInput) != 0 and not sqlInput.endswith(";"):
+				sqlInput += input(".. > ").strip("\r\n") # allow multi-line input
+			parse_line(sqlInput.split(";")[0])#,currentDb)
+			sqlInput = input().rstrip("\r\n")
+	except EOFError: # no more commands
+		pass # exit
+	print("Exiting")
+
+#This is the parser function which processes the input commands.
+def parse_line(line):
+	global currentDb
+	words = line.split()
+
+	if line[:2] == "--" or len(line) == 0:
+            pass #ignore
+	else:
+		try:
+			if words[0].lower() == "create": # found the command create
+				if words[1].lower() == "database":
+					if not db_exists(words[2]): # append to the list
+						currentDatabase = Database(words[2])
+						currentDb = words[2]
+						currentDatabase.create()
+						print("Database '"  + words[2] + "' created")
+					else:
+						print("!Failed to create database '" + words[2] + "' because it already exists")
+				elif words[1].lower() == "table":
+					if currentDb == "NA":
+						print("!Failed to create table '"+ words[2] +"' because no database is being used")
+					else:
+						if not tb_exists(words[2], currentDb):
+							currentTable = Table(words[2], currentDb)
+							currentTable.create()
+
+							currentWord = 3
+							wordCount = len(words)
+							nextWord = words[currentWord]
+
+							while currentWord < wordCount: # calls the add_column method on each argument
+								atrName = words[currentWord].strip("()")
+								if words[currentWord + 1][:3] == "int":
+									currentTable.add_column(int,atrName, 0)
+								elif words[currentWord + 1][:5] == "float":
+									currentTable.add_column(float,atrName, 0)
+								elif words[currentWord + 1][:4] == "char":
+									currentTable.add_column("char",atrName, words[currentWord + 1][words[currentWord + 1].find('(')+1:words[currentWord + 1].find(')')])
+								elif words[currentWord + 1][:7] == "varchar":
+									currentTable.add_column("varchar",atrName, words[currentWord + 1][words[currentWord + 1].find('(')+1:words[currentWord + 1].find(')')])
+								currentWord += 2
+							print("Table '" + words[2] + "' created")
+						else:
+							print("!Failed to create table '"+ words[2] +"' because it already exists")
+				else:
+					print('Syntax: "create <table | database> [(<tableAttrName> <tableAttrType>, ... )]"')
+
+			elif words[0].lower() == "drop":
+				if words[1].lower() == "table":
+					if currentDb == "NA":
+						print("!Failed to delete table '" + words[2] + "' because no database is being used")
+					elif tb_exists(words[2], currentDb):
+						currentTable = Table(words[2], currentDb)
+						currentTable.drop()
+						print("Table '" + words[2] + "' deleted")
+					else:
+						print("!Failed to delete table '" + words[2] + "' because it does not exist")
+				elif words[1].lower() == "database":
+					if db_exists(words[2]):# delete from list
+							if currentDb == words[2]: # dropping the current database
+								currentDb = "NA"
+							currentDb = Database(words[2])
+							currentDb.drop()
+							print("Database '" + words[2] + "' deleted")
+					else:
+						print("!Failed to delete database '" + words[2] + "' because it does not exist")
+
+			elif words[0].lower() == "select":
+				if currentDb == "NA":
+					print("!Failed to query table '" + words[3] + "' because no database is being used")
+				elif tb_exists(words[3], currentDb): # add words to select list until word = from
+					currentTable = Table(words[3], currentDb)
+					currentTable.select(words[1])
+				else:
+					print("!Failed to query table '" + words[3] + "' because it does not exist")
+
+			elif words[0].lower() == "use": # should make sure the db exist
+				if db_exists(words[1]):
+					currentDb = words[1]
+					print("Using database '" + words[1] + "'")
+				else:
+					print("!Failed to use database '" + words[2] + "' because it does not exist")
+
+			elif words[0].lower() == "alter":
+				if words[1].lower() == "table":
+					if currentDb == "NA":
+						print("!Failed to alter table '" + words[2] + "' because no database is being used")
+					elif words[3].lower() == "add":
+						if tb_exists(words[2], currentDb):
+							currentTable = Table(words[2], currentDb)
+							if words[5] == "int":
+								currentTable.add_column(int,words[4], 0)
+							elif words[5]== "float":
+								currentTable.add_column(float, words[4], 0)
+							elif words[5][:4] == "char":
+								currentTable.add_column("char", words[4], words[5][4:].strip("()"))
+							elif words[5][:7] == "varchar":
+								currentTable.add_column("varchar", words[4], words[5][7:].strip("()"))
+							print("Table '" + words[2] + "' modified")
+						else:
+							print("!Failed to alter table '" + words[2] + "' because it does not exist")
+				else:
+					print('Syntax: "alter table add <attrName> <attrType>"')
+
+			elif words[0].lower() == "insert":
+				if currentDb == "NA":
+						print("!Failed to insert to table '" + words[2] + "' because no database is being used")
+				elif words[1].lower() == "into":
+					if tb_exists(words[2], currentDb):
+						arguments = "".join(words[3:]).replace('(',',').replace(')','').split(',')
+						if arguments[0].lower() == "values":
+							currentTable = Table(words[2], currentDb)
+							if currentTable.insert(arguments[1:]):
+								print("1 new record inserted")
+						else:
+							print("Invalid insertion command: " + arguments[0])
+					else:
+						print("!Failed to insert into table '" + words[2] + "' because it does not exist")
+
+#			elif words[0].lower() == "update":
+#				if tb_exists(words[1], currentDb):
+#					if words[2].lower() == "set":
+#						print("update_database(set " + words[7] + " " + words[5] + " where " + words[7] + " = " + words[9] +")")
+#				else:
+#					print("!Failed to update table '" + words[2] + "' because it does not exist")
+			else:
+				print("Invalid line: " + line)
+		except IndexError:
+			print("Invalid line: " + line)
+
+def db_exists (name):
+    #any(current for current in dbName if current.name == name)
+	#should it return if the file exsists?
+	#print("in exist")
+	filePath = DIRECTORY+name+""
+	return os.path.exists(filePath)
+
+def tb_exists(tname, currentDb):
+	filePath = DIRECTORY+currentDb+"/"+tname+".txt"
+	return os.path.isfile(filePath)
+
+if __name__ == "__main__":
+	main()
--- table.py
+++ table.py
@@ -1,54 +1,78 @@
 import os
 
+DIRECTORY = "PA2/"
+
 class Table:
-# should the table be a list of lists?????
-    #constructor
-    #declares the variables: tnme, db and filePath
-    def __init__(self, tableName, db ):
-        #print("In the table constructor")
-        self.tname = tableName
-        self.db = db #name of the database the table is located in
-        self.filePath = "PA1/"+self.db+"/"+self.tname+".txt"
+# Handles table operations
+# Initialized with a table name, can perform various operations on the table
+
+	#constructor
+	#declares the variables: tnme, db and filePath
+	def __init__(self, tableName, db ):
+		self.tname = tableName
+		self.db = db #name of the database the table is located in
+		self.filePath = DIRECTORY+self.db+"/"+self.tname+".txt"
 
 
-    #Creates the file that will represent the table
-    def create(self):
-        #filePath= "PA1/"+self.db+"/"+self.tname+".txt"
-        open(self.filePath,"a+")
-        #print("create the table")
+	#Creates the file that will represent the table
+	def create(self):
+		#filePath= "PA1/"+self.db+"/"+self.tname+".txt"
+		open(self.filePath,"a")
+		#print("create the table")
 
 
+	#deletes the table file
+	def drop(self):
+		os.remove(self.filePath)
 
-    #deletes the table file
-    def drop(self):
-        os.remove(self.filePath)
+	#delete file and metadata from the metadata file
 
-        #delete file and metadata from the metadata file
+	# displays the table information
+	def select(self, selected):
+		tbFile = open(self.filePath, "r")
+		metadata = tbFile.readline()
+		attributes = metadata.split("|")
 
-# displays the table information
-    def select(self, selected):
-        tbFile = open(self.filePath, "r")
-        metadata = tbFile.readline()
-        attributes = metadata.split("|")
+		selectedMetaData = ""
 
-        selectedMetaData = ""
+		for attribute in attributes:
+			if len(attribute) > 0 and ((attribute.split(" "))[0] == selected or selected == "*"): # allows for selecting all or a single column
+				selectedMetaData += (attribute + " | ")
 
-        for attribute in attributes:
-            if len(attribute) > 0 and ((attribute.split(" "))[0] == selected or selected == "*"): # allows for selecting all or a single column
-                selectedMetaData += (attribute + " | ")
+		print(selectedMetaData)
 
-        print(selectedMetaData)
+	#inserts tuple into table
+	def insert(self, arguments):
+		tbFile = open(self.filePath, "r")
+		metadata = tbFile.readline()
+		attributes = metadata.split("|")
 
-#adds and attribute to the table
-    def add_column(self,type, name, length):
+		dataToAdd = "\n"
 
-        file = open(self.filePath,"a")
+		successfulInsert = True
 
-        if(type == int):
-            file.write(name +" int" + "|")
-        elif(type == float):
-            file.write(name+" float" + "|")
-        elif(type == "char"):
-            file.write(name+ " char("+ length +")" + "|")
-        else:
-            file.write(name + " varchar("+length+")" + "|")
+		if len(arguments) != len(attributes) - 1:
+			print("Invalid tuple length: " + str(len(arguments)) + " (there are " + str(len(attributes) - 1) + " attributes)")
+			successfulInsert = False
+		else:
+			for arg in arguments:
+				# type check
+				dataToAdd += arg + "|"
+
+		wrFile = open(self.filePath, "a")
+		wrFile.write(dataToAdd)
+		return successfulInsert
+
+	#adds and attribute to the table
+	def add_column(self,type, name, length):
+
+		file = open(self.filePath,"a")
+
+		if(type == int):
+			file.write(name +" int" + "|")
+		elif(type == float):
+			file.write(name+" float" + "|")
+		elif(type == "char"):
+			file.write(name+ " char("+ length +")" + "|")
+		else:
+			file.write(name + " varchar("+length+")" + "|")
